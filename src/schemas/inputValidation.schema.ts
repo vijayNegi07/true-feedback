36fdbf44d9a826230dbe8eeb60b9a2ea340edfc3
Vijay Negi <negiiviijay@gmail.com>
2026-07-31 18:58:54 +0530

    create schema and validation

---
+++ src/schemas/inputValidation.schema.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+
+export const usernameValidation = z
+    .string()
+    .min(3,"Usermae must be atleast 3 characters")
+    .max(20, "Username must not be more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/,"Special characters are not alolwed")
+
+export const signUpValidation = z.object({
+    username:usernameValidation,
+    email:z.email({message:"Invalid email address"}),
+    password:z.string().min(6,"Passoword should be atleast 6 characters")
+})
+
+export const sigInValidation = z.object({
+    username:z.string(),
+    email:z.email()
+})
+
+export const verifyCodeValidation = z.string().length(6,"Code must be 6 characters");
+
+export const acceptMessageValidation = z.boolean();
+
+export const messageValidation = z
+    .string()
+    .min(10, "Must be 10 characters atleast")
+    .max(300, "Must be under 300 characters");
+
