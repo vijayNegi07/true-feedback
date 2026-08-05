5ccaf10bfe71bff0979ba560f4c7fa61f7ea8023
Vijay Negi <negiiviijay@gmail.com>
2026-08-05 13:59:31 +0530

    add AI message generation endpoint and update dependencies

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,36 @@
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  createUIMessageStreamResponse,
+  toUIMessageStream,
+  generateText,
+} from 'ai';
+
+import {google} from "@ai-sdk/google"
+
+export async function POST(req: Request) {
+ try {
+
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const result = streamText({
+        model: google("gemini-3.6-flash"),
+        prompt,
+    });
+
+    // Wait for the full text if you need to save it to a database
+    const fullText = await result.text;
+
+    console.log(fullText);
+
+    // Meanwhile, return the stream to the client
+    return createUIMessageStreamResponse({
+        stream: toUIMessageStream({
+            stream: result.stream,
+        }),
+    });
+ } catch (error) {
+    console.log("Some error while using ai ", error);
+ }
+}
